85c7db3571f144df0c16e04acea96e8637550c24
Amir Shayegh <shayegh@>
2019-11-04 08:21:22 -0800

    added autosync

---
+++ ipad/Utilities/sync/Sync view/SyncView.swift
@@ -0,0 +1,24 @@
+//
+//  SyncView.swift
+//  ipad
+//
+//  Created by Amir Shayegh on 2019-11-04.
+//  Copyright © 2019 Amir Shayegh. All rights reserved.
+//
+
+import UIKit
+import Modal
+
+class SyncView: ModalView {
+    
+    public func initialize() {
+        setFixed(width: 350, height: 160)
+        style()
+        present()
+    }
+    
+    private func style() {
+        
+    }
+
+}
