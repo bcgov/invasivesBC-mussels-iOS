e7af2d7f7d6db6793419a073437f3963e4988029
Andrea Williams <andrea.williams@gov.bc.ca>
2019-11-08 16:25:47 -0800

    Fix button alignment & variable actions

--- ipad/Utilities/Sync/Sync view/SyncView.swift
+++ ipad/Utilities/Sync/Sync view/SyncView.swift
@@ -18,11 +18,18 @@ class SyncView: ModalView, Theme {
     @IBOutlet weak var syncImageView: UIImageView!
     @IBOutlet weak var titleView: UIView!
     @IBOutlet weak var statusIconView: UIView!
+    @IBOutlet weak var buttonView: UIView!
     @IBOutlet weak var hollowButton: UIButton!
     @IBOutlet weak var fillButton: UIButton!
     @IBOutlet weak var dividerView: UIView!
+
+    enum SyncState {
+        case inprogress
+        case complete
+        case error
+    }
     
-    
+    var syncState = SyncState.inprogress
     let syncIconTag = 200
     let invisibleAlpha: CGFloat = 0
     let visibleAlpha: CGFloat = 1
@@ -36,17 +43,22 @@ class SyncView: ModalView, Theme {
     @IBAction func cancelSyncAction(_ sender: Any) {
         self.remove()
     }
+    @IBAction func continueAction(_ sender: Any) {
+        if syncState == SyncState.complete {
+            self.remove()
+        }
+    }
     
     private func style() {
         styleCard(layer: self.layer)
         styleSectionTitle(label: titleLabel)
         titleLabel.textColor = Colors.primary
-        styleDivider(view: dividerView)
+        styleDividerGrey(view: dividerView)
         styleHollowButton(button: hollowButton)
         styleFillButton(button: fillButton)
-//        styleSyncInProgress()
+        styleSyncInProgress()
 //        styleSyncError()
-        styleSyncSuccess()
+//        styleSyncSuccess()
     }
     
     private func clearIcon() {
@@ -56,25 +68,55 @@ class SyncView: ModalView, Theme {
     }
     
     private func styleSyncInProgress() {
+        syncState = SyncState.inprogress
         statusLabel.text = "Now syncing your data..."
         hollowButton.setTitle("Cancel Sync", for: .normal)
         fillButton.isHidden = true
+        hollowButton.isHidden = false
+        
+        buttonView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hollowButton.centerXAnchor.constraint(equalTo: buttonView.centerXAnchor),
+            hollowButton.centerYAnchor.constraint(equalTo: buttonView.centerYAnchor),
+        ])
+        
         showSyncInProgressAnimation()
     }
     
     private func styleSyncSuccess() {
+        syncState = SyncState.complete
         statusLabel.text = "Syncing Complete"
         hollowButton.isHidden = true
         fillButton.isHidden = false
+        
+        buttonView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            fillButton.centerXAnchor.constraint(equalTo: buttonView.centerXAnchor),
+            fillButton.centerYAnchor.constraint(equalTo: buttonView.centerYAnchor),
+        ])
+        
         fillButton.setTitle("Continue", for: .normal)
         showSyncCompletedAnimation()
     }
     
     private func styleSyncError() {
+        syncState = SyncState.error
         statusLabel.text = "There was an error while syncing your data"
         hollowButton.setTitle("Skip for now...", for: .normal)
         fillButton.setTitle("Try again", for: .normal)
         fillButton.isHidden = false
+        hollowButton.isHidden = false
+        
+        buttonView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            fillButton.centerXAnchor.constraint(equalTo: buttonView.centerXAnchor),
+            hollowButton.centerXAnchor.constraint(equalTo: buttonView.centerXAnchor),
+            fillButton.topAnchor.constraint(equalTo: buttonView.topAnchor),
+            hollowButton.bottomAnchor.constraint(equalTo: buttonView.bottomAnchor),
+        ])
+        
+        fillButton.center.x = buttonView.center.x
+        hollowButton.center.x = buttonView.center.x
         showSyncFailedAnimation()
     }
     
