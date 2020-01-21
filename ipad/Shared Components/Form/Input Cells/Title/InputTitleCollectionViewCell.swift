909a171035dcb86e0f90099f3582bbf05dd867b6
Amir Shayegh <shayegh@me.com>
2020-01-21 11:32:34 -0800

    added title input component for adding field headers within an input group + removed 2 fields from watercraft inspection

---
+++ ipad/Shared Components/Form/Input Cells/Title/InputTitleCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  InputTitleCollectionViewCell.swift
+//  ipad
+//
+//  Created by Amir Shayegh on 2020-01-21.
+//  Copyright © 2020 Amir Shayegh. All rights reserved.
+//
+
+import UIKit
+
+class InputTitleCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
