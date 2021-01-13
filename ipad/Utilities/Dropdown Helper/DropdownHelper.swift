972f26594429d41af04ef564a1126396cb466dcb
Warren <samw@WDSDeploys-MacBook-Pro.local>
2021-01-13 12:29:26 -0800

    Sort provinces by country Canada -> USA -> Mexico

--- ipad/Utilities/Dropdown Helper/DropdownHelper.swift
+++ ipad/Utilities/Dropdown Helper/DropdownHelper.swift
@@ -27,11 +27,11 @@ class DropdownHelper {
     
     public func getDropdown(for type: CodeTableType) -> [DropdownModel] {
         let items = Storage.shared.codeTable(type: type)
-        return dropdown(from: items)
+        return dropdown(from: items, sort: false)
     }
     
     public func getDropDownObject(for type: CodeTableType) -> [DropdownModel] {
-        return dropdown(from: Storage.shared.codeObjects(type: type))
+        return dropdown(from: Storage.shared.codeObjects(type: type), sort: false)
     }
     
     public func getDropDownCodes(for type: CodeTableType ) -> [CodeObject] {
@@ -39,16 +39,23 @@ class DropdownHelper {
     }
     
     public func getDropdownForPrvinces() -> [DropdownModel] {
-        return dropdown(from: Storage.shared.provinces())
+        return dropdown(from: Storage.shared.provinces(), sort: true)
     }
     
     public func getProvinceCodes() -> [CountryProvince] {
         return Storage.shared.provincesCodes()
     }
     
-    public func dropdown(from items: [String]) -> [DropdownModel] {
+    public func dropdown(from items: [String], sort: Bool) -> [DropdownModel] {
+        let sort = true;
         var options: [DropdownModel] = []
-        let set = Array(Set(items)).sorted()
+        var set = Array(Set(items))
+        if (sort) {
+            let CAN = set.filter({$0.contains("CAN")}).sorted()
+            let USA = set.filter({$0.contains("USA")}).sorted()
+            let MEX = set.filter({$0.contains("MEX")}).sorted()
+            set = CAN + USA + MEX
+        }
         for item in set {
             options.append(DropdownModel(display: item))
         }
