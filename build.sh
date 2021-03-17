4d0a4e7326bcfd659fbe34a242911dc6034ec421
Micheal Wells <micheal.w.wells@gov.bc.ca>
2021-03-17 09:13:06 -0700

    shell scripts for manual build, packing for mobile signing, and upload to test flight

---
+++ build.sh
@@ -0,0 +1,1 @@
+sudo xcodebuild -allowProvisioningUpdates -workspace ipad.xcworkspace -scheme ipad -configuration Release clean archive -archivePath buildArchive/App.xcarchive CODE_SIGN_IDENTITY="Range BC - Invasive Species" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO
