53cdc2dc17a004c7f8ecabf33a83dd88e1956dcc
Sam Warren <samwarren4@gmail.com>
2021-03-17 18:36:15 -0700

    Fix undefined bug

--- ipad/Models/Waterfract Inspection/PreviousWaterBodyModel.swift
+++ ipad/Models/Waterfract Inspection/PreviousWaterBodyModel.swift
@@ -84,7 +84,7 @@ class PreviousWaterbodyModel: JourneyModel, BaseRealmObject {
         if !self.otherWaterbody.isEmpty {
             return [
                 "journeyType": 1,
-                "numberOfDaysOut": numberOfDaysOut,
+                "numberOfDaysOut": numberOfDaysOut.count > 0 ? numberOfDaysOut : "N/A",
                 "otherWaterBody": self.otherWaterbody
             ]
         }
@@ -95,7 +95,7 @@ class PreviousWaterbodyModel: JourneyModel, BaseRealmObject {
         
         return [
             "journeyType": 1,
-            "numberOfDaysOut": numberOfDaysOut,
+            "numberOfDaysOut": numberOfDaysOut.count > 0 ? numberOfDaysOut : "N/A",
             "waterBody": remoteId
         ]
     }
