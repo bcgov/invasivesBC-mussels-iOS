9ef89e504a56353173d1fdc603f2b7cf763fdb17
Micheal Wells <micheal.w.wells@gov.bc.ca>
2021-03-25 09:00:39 -0700

    tweaks for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 unzip signed.zip
-/Applications/Transporter.app/Contents/itms/bin/iTMSTransporter -m upload -u $APP_STORE_USERNAME -p $APP_SPECIFIC_PASSWORD -itc_provider HerMajestytheQueeninrightoftheProvinceofBritishColumbia -assetFile App/App.ipa
+/Applications/Transporter.app/Contents/itms/bin/iTMSTransporter -m upload -u $APP_STORE_USERNAME -p $APP_SPECIFIC_PASSWORD -itc_provider HerMajestytheQueeninrightoftheProvinceofBritishColumbia -assetFile App/Inspect.ipa
