39d71f868f90f50389af79265b6f21d6e9a80c83
Dave Claveau <daveclaveau@gmail.com>
2023-02-10 17:15:58 -0800

    fix layout of button change and dependents

--- ipad/Models/Waterfract Inspection/Form Fields/WatercraftInspectionFormHelper.swift
+++ ipad/Models/Waterfract Inspection/Form Fields/WatercraftInspectionFormHelper.swift
@@ -253,7 +253,7 @@ class WatercraftInspectionFormHelper {
             header: WatercraftFieldHeaderConstants.WatercraftDetails.previousInspection,
             editable: editable ?? true,
             value: object?.previousInspection ?? nil,
-            width: .Full,
+            width: .Third,
             validationName: .previousInspectionInteracted,
             interacted: object?.previousInspectionInteracted ?? false
         )
@@ -265,7 +265,7 @@ class WatercraftInspectionFormHelper {
             header: WatercraftFieldHeaderConstants.WatercraftDetails.previousInspectionSource,
             editable: editable ?? true,
             value: object?.previousInspectionSource ?? nil,
-            width: .Full,
+            width: .Half,
             dropdownItems: DropdownHelper.shared.getDropDownObject(for: .previousInspectionSource),
             codes: DropdownHelper.shared.getDropDownCodes(for: .previousInspectionSource)
         )
