eeaa09f97acd5b80fdb24ef8379e886517708735
PaulGarewal <paul.garewal@gov.bc.ca>
2025-06-13 13:52:57 -0700

    feat: added new station list and sorting

--- ipad/Utilities/Dropdown Helper/DropdownHelper.swift
+++ ipad/Utilities/Dropdown Helper/DropdownHelper.swift
@@ -27,7 +27,47 @@ class DropdownHelper {
     
     public func getDropdown(for type: CodeTableType) -> [DropdownModel] {
         let items = Storage.shared.codeTable(type: type)
+        
+        // We'll be using custom sorting for the station list, as per service owner request
+        if type == .stations {
+            return dropdownWithCustomStationSort(from: items)
+        }
+        
         return dropdown(from: items, sort: false)
+    }
+    
+    // Custom dropdown sorting function, used to put each station in a category, then sort each category alphabetically
+    private func dropdownWithCustomStationSort(from items: [String]) -> [DropdownModel] {
+        let uniqueItems = Array(Set(items))
+        
+        // Categorize stations (permanent, various roving stations, and the special stations (other, emergency and project))
+        let permanentStations = uniqueItems.filter { isPermanentStation($0) }.sorted()
+        let lowerMainlandRoving = uniqueItems.filter { $0.contains("Lower Mainland Roving") }.sorted()
+        let pacificBorderRoving = uniqueItems.filter { $0 == "Pacific Border" }
+        let pentictonRoving = uniqueItems.filter { $0.contains("Penticton Roving") }.sorted()
+        let cranbrookRoving = uniqueItems.filter { $0.contains("Cranbrook Roving") }.sorted()
+        let sumasRoving = uniqueItems.filter {$0 == "Sumas" }
+        let specialStations = uniqueItems.filter { isSpecialStation($0) }.sorted()
+        
+        // Combine all of our stations in the desired order
+        let sortedItems = permanentStations + lowerMainlandRoving + pacificBorderRoving + pentictonRoving + cranbrookRoving + sumasRoving + specialStations
+        
+        // Convert to DropdownModel objects
+        return sortedItems.map { DropdownModel(display: $0) }
+    }
+    
+    // Check if a station is a permanent, this will have to be adjusted when the station list changes in the future along with the other checks
+    private func isPermanentStation(_ station: String) -> Bool {
+        let permanentStations = [
+            "Cutts (Hwy 93)", "Dawson Creek", "Golden", "Mt. Robson", 
+            "Olsen (Hwy 3)", "Osoyoos", "Radium", "Yahk"
+        ]
+        return permanentStations.contains(station)
+    }
+    
+    private func isSpecialStation(_ station: String) -> Bool {
+        let specialStations = ["Emergency Response", "Other", "Project"]
+        return specialStations.contains(station)
     }
     
     public func getDropDownObject(for type: CodeTableType) -> [DropdownModel] {
